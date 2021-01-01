4a90e689b6b882ce917a0084c0b5e9f528635dc7
nicoelbert <30658685+nicoelbert@users.noreply.github.com>
2021-01-01 22:04:54 +0100

    Update classes.py

--- classes/classes.py
+++ classes/classes.py
@@ -3,8 +3,8 @@
 class Depot:
     def __init__(self,name,lon,lat):
         self.name = name
-        self lon = lon
-        self lat = lat 
+        self.lon = lon
+        self.lat = lat 
 
 ##########################################################################
 
