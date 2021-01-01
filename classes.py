3760c653853d0d99823936109e84eb817ab53093
nicoelbert <30658685+nicoelbert@users.noreply.github.com>
2021-01-01 21:18:40 +0100

    Basic classes created

---
+++ classes.py
@@ -0,0 +1,36 @@
+class Depot:
+    def __init__(self,name,lon,lat):
+        self.name = name
+        self lon = lon
+        self lat = lat 
+
+##########################################################################
+
+class Plant:
+    def __init__(self,name,lon,lat):
+        self.name = name
+        self.lon = lon
+        self.lat = lat
+
+
+##########################################################################
+
+class Site: 
+    def __init__(self,key,zip,lon,lat)
+        self.key = str(zip) + str(lon) + str(lat)
+        self.zip = zip
+        self.lon = lon
+        self.lat = lat
+
+##########################################################################
+
+class Job: 
+    def __init__(self,id,key,plant,silo,material,start,duration,site):
+        self.id = id
+        self.key = key
+        self.plant = plant
+        self.silo = silo
+        self.material = material
+        self.start = start
+        self.duration = duration
+        self.site = site
