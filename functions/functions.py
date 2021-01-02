2fa17d422d796fd074fd86930edca7f58978d8f8
nicoelbert <30658685+nicoelbert@users.noreply.github.com>
2021-01-02 22:43:30 +0100

    update Site in classes.py

--- functions/functions.py
+++ functions/functions.py
@@ -1,6 +1,5 @@
 import math
 
-
 def get_distance(point1,point2):
     #source : https://www.kite.com/python/answers/how-to-find-the-distance-between-two-lat-long-coordinates-in-python
     R = 6373.0 #radius of the earth
@@ -19,3 +18,39 @@ def get_distance(point1,point2):
     distance = R * c
 
     return(distance)
+
+
+#######################################################################
+
+def getMap(list_depots,list_plants,list_sites):
+    import matplotlib.pyplot as plt
+
+    list_depot_lon = []
+    list_depot_lat = []
+
+    list_plant_lon = []
+    list_plant_lat = []
+
+    list_sites_lon = []
+    list_sites_lat = []
+
+    for d in list_depots:
+        list_depot_lon.append(d.lon)
+        list_depot_lat.append(d.lat)
+
+    for p in list_plants:
+        list_plant_lon.append(p.lon)
+        list_plant_lat.append(p.lat)
+
+    for s in list_sites:
+        list_sites_lon.append(s.lon)
+        list_sites_lat.append(s.lat)
+
+    fig = plt.figure(figsize=(12, 8), dpi=100, facecolor='w', edgecolor='k')
+
+    plt.scatter(list_sites_lon, list_sites_lat, marker='.', color='grey')
+    plt.scatter(list_depot_lon, list_depot_lat, 100, color='blue')
+    plt.scatter(list_plant_lon, list_plant_lat, 100, marker='X', color='green')
+
+    plt.show()
+
