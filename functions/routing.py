bc1b0a40276662fdb6a90ba42745c8a2bce09247
nicoelbert <30658685+nicoelbert@users.noreply.github.com>
2021-01-04 05:13:12 +0100

    update routing

--- functions/routing.py
+++ functions/routing.py
@@ -28,40 +28,81 @@ def get_distance(point1, point2):
 #!pip install pulp
 from pulp import *
 
+
 def routing(tour: cl.Tour):
-    #double plants need to be handled
-    depot_node = tour.depot
-    plant_node = tour.list_plants[0] #only first plant is selected
+    # double plants need to be handled
 
-    dropoff_nodes = tour.list_dropoffs
-    pickup_nodes = tour.list_pickups
+    # create working nodes to level different object types
+    depot_node = cl.Worknode('depot', tour.depot.name, tour.depot)
+    plant_node = cl.Worknode('plant', tour.list_plants[0].name, tour.list_plants[0])  # only first plant is selected
 
-    #combined lists
+    # format jobs to sites
+    dropoff_nodes = []
+    pickup_nodes = []
+
+    for j in tour.list_dropoffs:
+        wn_j = cl.Worknode('dropoff_job', j.name, j.site)
+        dropoff_nodes.append(wn_j)
+    for j in tour.list_pickups:
+        wn_j = cl.Worknode('pickup_job', j.name, j.site)
+        pickup_nodes.append(wn_j)
+
+    # combined lists
     site_nodes = dropoff_nodes + pickup_nodes
-    vis_nodes = site_nodes
-    vis_nodes.append(plant_node)
-    all_nodes = vis_nodes
+    visitable_nodes = site_nodes
+    visitable_nodes.append(plant_node)
+    all_nodes = visitable_nodes
     all_nodes.append(depot_node)
 
-    #create enough timeslots at least one plantslot after each site
+    for n in pickup_nodes:
+        if n in dropoff_nodes:
+            print("error with %s" % n)
+
+    # create enough timeslots at least one plantslot after each site
     i_timeslots = len(site_nodes) * 2
     timeslots = []
 
     for i in range(i_timeslots):
         timeslots.append(i)
 
-    #create model
+    # create distance dict
+    distances = {}
+    for n in all_nodes:
+        distances[n] = {}
+
+    for i in all_nodes:
+        for j in all_nodes:
+            distances[i][j] = rt.get_distance(i, j)
+
+    # create model
     m = LpProblem("Routing", LpMinimize)
 
     # create Variables
     x = LpVariable.dicts('edge', (all_nodes, all_nodes, timeslots), cat='Binary')
+    y = LpVariable.dicts('load_truck', (timeslots), cat='Binary')
+    z = LpVariable.dicts('load_silo', (timeslots), cat='Binary')
 
+    # define constraints
 
+    # a node cannot have an edge with itself
+    for i in visitable_nodes:
+        m += LpAffineExpression([(x[i][i][t], 1) for t in timeslots]) == 0, 'no_selfvisit_%s' % i
+
+    # flow in and out constraints node
+    for j in site_nodes:
+        m += LpAffineExpression(
+            [(x[i][j][t], 1) for t in timeslots for i in site_nodes if i != j]) == 1, 'flow_in_%s' % j
+        m += LpAffineExpression(
+            [(x[j][i][t], 1) for t in timeslots for i in site_nodes if i != j]) == 1, 'flow_out_%s' % j
+
+    # sites and plant need to be left in subsequent timeslot after entering # not for the last timeslot to avoid key errors, should be handled by depot constraints
+    for v in visitable_nodes:
+        for t in range(len(timeslots) - 1):
+            m += LpAffineExpression([(x[i][v][t], 1) for i in all_nodes if i != v]) == LpAffineExpression(
+                [(x[v][j][t + 1], 1) for j in all_nodes if j != v]), 'subsequent_ts_in_out_v{}_t{}'.format(v.name, t)
 
     # Objective
-    m += LpAffineExpression([(x[i][j][t], 1) for i in all_nodes for j in all_nodes for t in timeslots])
-
-
+    m += LpAffineExpression([(x[i][j][t], distances[i][j]) for i in all_nodes for j in all_nodes for t in timeslots])
 
     m.solve()
     print(m.status)
