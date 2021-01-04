365fd0e20583a8b921a6f26adeebbd09dad6306d
nicoelbert <30658685+nicoelbert@users.noreply.github.com>
2021-01-04 22:40:22 +0100

    update routing

--- functions/functions.py
+++ functions/functions.py
@@ -50,3 +50,10 @@ def get_time():
 def save_object(obj, filename):
     with open(filename, 'wb') as output:  # Overwrites any existing file.
         pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
+
+def load_object(filename):
+    with open(filename, 'rb') as input:
+        loaded_object = pickle.load(input)
+    return loaded_object
+
+########################################################################
